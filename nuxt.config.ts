724dca85fb459c31f7f3376974ab7ae9eb0ba26c
cyprientertrais <ctertrais@partner.auchan.fr>
2025-05-17 07:01:34 +0200

    feat: :sparkle: push static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+  app: {
+    baseURL: '/zikmu/',
+  },
   vite: {
     plugins: [
       tailwindcss(),
